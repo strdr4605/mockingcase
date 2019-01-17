8e6c9130b6432c20b3a87bda114e64c2fba38cf6
Dragos Strainu <str.dr4605@gmail.com>
2019-01-17 22:42:34 +0200

    Close #14 Add declaration file for typescript

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for mockingcase 1.1
+// Project: https://github.com/strdr4605/mockingcase
+// Definitions by: Dragoș Străinu https://github.com/strdr4605
+
+declare module 'mockingcase' {
+    export = mockingcase;
+    function mockingcase(input: string, options?: {random?: boolean}): string;
+    namespace mockingcase {}
+}
