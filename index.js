6e6f39d680509ab99fdd66f1a0efe4aff4e37d02
Dragos Strainu <str.dr4605@gmail.com>
2019-01-28 11:31:21 +0200

    Use the function form of 'use strict'

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-'use strict';
+(function () {
+  'use strict';
 
-/**
- * This function receives a string input and converts it to mOcKiNgCaSe.
- * @param {string} input - string to be converted
- * @param {object} [options={random: false}] - options for converting
- * @param {boolean} options.random=false - using random for converting
- * @returns {string} string in mOcKiNgCaSe
- */
-function mOcKiNgCaSe(input, options) {
-  options = Object.assign({
-    random: false
-  }, options);
+  /**
+   * This function receives a string input and converts it to mOcKiNgCaSe.
+   * @param {string} input - string to be converted
+   * @param {object} [options={random: false}] - options for converting
+   * @param {boolean} options.random=false - using random for converting
+   * @returns {string} string in mOcKiNgCaSe
+   */
+  function mOcKiNgCaSe(input, options) {
+    options = Object.assign({
+      random: false
+    }, options);
 
-  return input.replace(/./g, (s, i) => {
-    if (options.random) return Math.round(Math.random()) ? s.toUpperCase() : s.toLowerCase()
-    else return i % 2 ? s.toUpperCase() : s.toLowerCase()
-  });
-}
+    return input.replace(/./g, (s, i) => {
+      if (options.random) return Math.round(Math.random()) ? s.toUpperCase() : s.toLowerCase()
+      else return i % 2 ? s.toUpperCase() : s.toLowerCase()
+    });
+  }
 
-module.exports = mOcKiNgCaSe;
+  module.exports = mOcKiNgCaSe;
+})();
