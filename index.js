a364836ac6a1230a0eb4ea4797d9b2f0864abb4b
Akalanka Weerasooriya <mailboxaaw@gmail.com>
2019-03-25 19:45:47 +0530

    added onlyLetters option, and update the readme.

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-'use strict';
+"use strict";
 
 /**
  * This function receives a string input and converts it to mOcKiNgCaSe.
  * @param {(string | string[])} input - string or array of strings to be converted
  * @param {object} [options={random: false}] - options for converting
  * @param {boolean} options.random=false - using random for converting
+ * @param {boolean} options.onlyLetters=false - using only letters of the string
  * @returns {string} string in mOcKiNgCaSe
  */
 function mOcKiNgCaSe(input = "", options) {
-  options = Object.assign({
-    random: false,
-  }, options);
+  options = Object.assign(
+    {
+      random: false,
+      onlyLetters: false
+    },
+    options
+  );
 
   //First, check to see that an input is recieved, and throw error if not
-  if(input === ""){
-    throw new Error("An input is required")
+  if (input === "") {
+    throw new Error("An input is required");
   }
 
-  if (isArrayOfStrings(input)) input = input.join('');
+  if (isArrayOfStrings(input)) input = input.join("");
+
+  //Checks the onlyLetters option, and remove numbers if there are any
+  if (options.onlyLetters) {
+    input = input.replace(/[^a-zA-Z]/g, "");
+  }
 
   return input.replace(/./g, (s, i) => {
-    if (options.random) return Math.round(Math.random()) ? s.toUpperCase() : s.toLowerCase();
+    if (options.random)
+      return Math.round(Math.random()) ? s.toUpperCase() : s.toLowerCase();
     else return i % 2 ? s.toUpperCase() : s.toLowerCase();
   });
 }
@@ -33,15 +44,18 @@ function mOcKiNgCaSe(input = "", options) {
  */
 mOcKiNgCaSe.log = (input, options) => console.log(mOcKiNgCaSe(input, options));
 
-const isArrayOfStrings = (input) => {
+const isArrayOfStrings = input => {
   if (Array.isArray(input)) {
     input.forEach((value, i) => {
-      if (typeof value !== 'string') throw TypeError(`Expected array of strings but got type '${typeof value}' at  index ${i}`);
+      if (typeof value !== "string")
+        throw TypeError(
+          `Expected array of strings but got type '${typeof value}' at  index ${i}`
+        );
     });
     // If no error found returns true
     return true;
   }
-}
+};
 
 /**
  * Creates `String.prototype.toMockingCase()`.
