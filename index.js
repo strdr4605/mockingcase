4eda8971dca81cd079310ef76bf5bb6527e5ee25
Rafael09ED <starmarstrek@gmail.com>
2019-04-26 23:10:13 -0400

    removed extra code

--- index.js
+++ index.js
@@ -131,27 +131,4 @@ function convert(input, shouldLetterBeUpperCase) {
   });
 }
 
-function overrideConsole(){
-  let console = window.console;
-  if (!console) return;
-  function intercept(method){
-    let original = console[method];
-    console[method] = function(){
-      // do sneaky stuff
-      if (original.apply){
-        // Do this for normal browsers
-        original.apply(console, arguments);
-      }else{
-        // Do this for IE
-        var message = Array.prototype.slice.apply(arguments).join(' ');
-        original(message);
-      }
-    }
-  }
-  var methods = ['log', 'warn', 'error'];
-  for (var i = 0; i < methods.length; i++){
-    intercept(methods[i]);
-  }
-}
-
 module.exports = mOcKiNgCaSe;
