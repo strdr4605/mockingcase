554518dcd9fc0f8f7541cd7dce18a2d70ced6ef6
Anmol Varma <anmol_varma@github.com>
2019-08-16 19:18:00 +0530

    Moved jsdocs before function declaration

--- index.js
+++ index.js
@@ -99,17 +99,17 @@ mOcKiNgCaSe.overrideString = () => {
   return mOcKiNgCaSe;
 };
 
+/**
+ * Overrides console.log input to print the input mOcKiNgCaSe.
+ * @param {object} [options={random: false}] Options for converting.
+ * @param {boolean} options.random=false - If case conversion should be randomized.
+ * @param {boolean} options.onlyLetters=false - If non letters characters should be removed.
+ * @param {boolean} options.firstUpper=false - If the first letter should be capitalized instead of the second when converting to mOcKiNgCaSe (e.g. MoCkInGcAsE).
+ * When combined with `options.random`, the first letter of the random string will be capitalized.
+ * @returns {string} string in mOcKiNgCaSe
+ * @see mOcKiNgCaSe
+ */
 mOcKiNgCaSe.overrideConsole = (options = {}) => {
-  /**
-   * Overrides console.log input to print the input mOcKiNgCaSe.
-   * @param {object} [options={random: false}] Options for converting.
-   * @param {boolean} options.random=false - If case conversion should be randomized.
-   * @param {boolean} options.onlyLetters=false - If non letters characters should be removed.
-   * @param {boolean} options.firstUpper=false - If the first letter should be capitalized instead of the second when converting to mOcKiNgCaSe (e.g. MoCkInGcAsE).
-   * When combined with `options.random`, the first letter of the random string will be capitalized.
-   * @returns {string} string in mOcKiNgCaSe
-   * @see mOcKiNgCaSe
-   */
   const print = console.log;
   console.log = value => {
     print(mOcKiNgCaSe(value, options));
