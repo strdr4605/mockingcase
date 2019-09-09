0bf409dca02a94bb97ebe0c63efc11ac369b453d
Max Rumsey <max@maxrumsey.xyz>
2019-09-10 03:34:31 +0800

    CDN Support (#56)
    * Adding support for CDN usage.
    
    * Fixing testing issues.
    
    * Fixing readme issues.
    
    * Adding docs

--- index.js
+++ index.js
@@ -148,5 +148,6 @@ function randomCase(input) {
 function convert(input, shouldLetterBeUpperCase) {
   return input.replace(/./g, (str, i) => (shouldLetterBeUpperCase(str, i) ? str.toUpperCase() : str.toLowerCase()));
 }
-
-module.exports = mOcKiNgCaSe;
+if (typeof window === 'undefined') {
+  module.exports = mOcKiNgCaSe;
+}
