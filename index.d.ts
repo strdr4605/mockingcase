243714e574d7fc6f9d7a796b864e653e6652ced3
Chinedum Ukejianya <cukejianya@gmail.com>
2019-09-13 20:00:41 -0400

    Fix typescript

--- index.d.ts
+++ index.d.ts
@@ -7,8 +7,8 @@ declare module "mockingcase" {
     random?: boolean;
     onlyLetters?: boolean;
     firstUpper?: boolean;
-    upper?: string | object;
-    lower?: string | object;
+    upper?: string | RegExp;
+    lower?: string | RegExp;
   }
 
   export = mockingcase;
