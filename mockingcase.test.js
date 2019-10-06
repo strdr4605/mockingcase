67600fab1c9a237c61fc8c55a4e6f1c247b4637a
Sarvesh Dubey <38752758+dubesar@users.noreply.github.com>
2019-10-06 17:26:38 +0530

    Made changes in test file
    Changed all mOcKiNgCaSe in mockingcase in test file.

--- mockingcase.test.js
+++ mockingcase.test.js
@@ -3,20 +3,20 @@
  */
 /* eslint-disable no-shadow */
 /* eslint-disable global-require */
-const mOcKiNgCaSe = require("./index").overrideString();
+const mockingcase = require("./index").overrideString();
 
 describe("mockingcase", () => {
   describe("input as string", () => {
     test("Should convert even index letters to lowercase and odd index letters to uppercase", () => {
       const input = "hello world";
       const expectedOutput = "hElLo wOrLd";
-      expect(mOcKiNgCaSe(input)).toEqual(expectedOutput);
+      expect(mockingcase(input)).toEqual(expectedOutput);
     });
 
     test("Should only modify letters", () => {
       const input = "1234567890hello world!@#$%^&*()_+[]{},./<>?`~\\";
       const expectedOutput = "1234567890hElLo wOrLd!@#$%^&*()_+[]{},./<>?`~\\";
-      expect(mOcKiNgCaSe(input)).toEqual(expectedOutput);
+      expect(mockingcase(input)).toEqual(expectedOutput);
     });
   });
 
@@ -25,8 +25,8 @@ describe("mockingcase", () => {
     const options = {
       random: true,
     };
-    const normalOutput = mOcKiNgCaSe(input);
-    const randomlyConvertedOutput = mOcKiNgCaSe(input, options);
+    const normalOutput = mockingcase(input);
+    const randomlyConvertedOutput = mockingcase(input, options);
 
     test("Returned string should equal input string when ignoring case", () => {
       expect(normalOutput.toLowerCase()).toStrictEqual(randomlyConvertedOutput.toLowerCase());
@@ -39,7 +39,7 @@ describe("mockingcase", () => {
       };
       // Generate 100 random strings and ensure all have an uppercase first letter.
       for (let i = 0; i < 100; i++) {
-        const output = mOcKiNgCaSe(input, options);
+        const output = mockingcase(input, options);
         expect(output[0]).toStrictEqual(output[0].toUpperCase());
       }
     });
@@ -50,7 +50,7 @@ describe("mockingcase", () => {
     const options = {
       firstUpper: true,
     };
-    const output = mOcKiNgCaSe(input, options);
+    const output = mockingcase(input, options);
     const expectedOutput = "HeLlO WoRlD";
 
     test("Should convert every other letter to uppercase, starting with the first letter", () => {
@@ -61,33 +61,33 @@ describe("mockingcase", () => {
   describe("empty input", () => {
     test("If the input is left blank, an error should be thrown", () => {
       const input = "";
-      expect(() => mOcKiNgCaSe(input)).toThrowError(new Error("An input is required"));
+      expect(() => mockingcase(input)).toThrowError(new Error("An input is required"));
     });
   });
 
   describe("input as array", () => {
-    test("If input is an array of strings, it should return the mOcKiNgCaSe version of the string formed by the array elements.", () => {
+    test("If input is an array of strings, it should return the mockingcase version of the string formed by the array elements.", () => {
       const input = ["foo", "bar"];
       const expectedOutput = "fOoBaR";
-      expect(mOcKiNgCaSe(input)).toEqual(expectedOutput);
+      expect(mockingcase(input)).toEqual(expectedOutput);
     });
 
     test("If input is an array but all elements aren't strings, it should throw an error.", () => {
       const input = ["foo", "1", 1];
-      expect(() => mOcKiNgCaSe(input)).toThrow();
+      expect(() => mockingcase(input)).toThrow();
     });
   });
 
   describe("#log", () => {
     test("log should send converted string to console.log", () => {
       const input = "hello world";
-      const expectedOutput = mOcKiNgCaSe(input);
+      const expectedOutput = mockingcase(input);
       let consoleOutput = null;
 
       console.log = jest.fn(output => {
         consoleOutput = output;
       });
-      mOcKiNgCaSe.log(input);
+      mockingcase.log(input);
 
       expect(consoleOutput).toEqual(expectedOutput);
     });
@@ -96,7 +96,7 @@ describe("mockingcase", () => {
   describe("toMockingCase", () => {
     test("Properly converts a string to mocking case", () => {
       const input = "hello world";
-      const expectedOutput = mOcKiNgCaSe(input);
+      const expectedOutput = mockingcase(input);
       expect(input.toMockingCase()).toEqual(expectedOutput);
     });
 
@@ -105,7 +105,7 @@ describe("mockingcase", () => {
       const options = {
         firstUpper: true,
       };
-      const expectedOutput = mOcKiNgCaSe(input, options);
+      const expectedOutput = mockingcase(input, options);
       expect(input.toMockingCase(options)).toMatch(expectedOutput);
     });
 
@@ -122,7 +122,7 @@ describe("mockingcase", () => {
         onlyLetters: true,
       };
       const expectedOutput = "hElLo wOrLd";
-      expect(mOcKiNgCaSe(input, options)).toEqual(expectedOutput);
+      expect(mockingcase(input, options)).toEqual(expectedOutput);
     });
 
     test("string with symbols", () => {
@@ -131,29 +131,29 @@ describe("mockingcase", () => {
         onlyLetters: true,
       };
       const expectedOutput = "hElLo wOrLd";
-      expect(mOcKiNgCaSe(input, options)).toEqual(expectedOutput);
+      expect(mockingcase(input, options)).toEqual(expectedOutput);
     });
   });
 
   describe("default options when importing/requiring package", () => {
     test("default options", () => {
-      const mOcKiNgCaSe = require("./index").config({
+      const mockingcase = require("./index").config({
         onlyLetters: true,
       });
       const input = "hello123";
       const expectedOutput = "hElLo";
-      expect(mOcKiNgCaSe(input)).toEqual(expectedOutput);
+      expect(mockingcase(input)).toEqual(expectedOutput);
     });
 
     test("NO default options", () => {
-      const mOcKiNgCaSe = require("./index").config();
+      const mockingcase = require("./index").config();
       const input = "hello123";
       const expectedOutput = "hElLo123";
-      expect(mOcKiNgCaSe(input)).toEqual(expectedOutput);
+      expect(mockingcase(input)).toEqual(expectedOutput);
     });
 
     test("override default options", () => {
-      const mOcKiNgCaSe = require("./index").config({
+      const mockingcase = require("./index").config({
         onlyLetters: true,
       });
       const input = "hello123";
@@ -161,11 +161,11 @@ describe("mockingcase", () => {
         onlyLetters: false,
       };
       const expectedOutput = "hElLo123";
-      expect(mOcKiNgCaSe(input, options)).toEqual(expectedOutput);
+      expect(mockingcase(input, options)).toEqual(expectedOutput);
     });
 
     test("overriding default options should keep unaffected defaults", () => {
-      const mOcKiNgCaSe = require("./index").config({
+      const mockingcase = require("./index").config({
         onlyLetters: true,
         firstUpper: true,
       });
@@ -174,49 +174,49 @@ describe("mockingcase", () => {
         onlyLetters: false,
       };
       const expectedOutput = "HeLlO123";
-      expect(mOcKiNgCaSe(input, options)).toEqual(expectedOutput);
+      expect(mockingcase(input, options)).toEqual(expectedOutput);
     });
   });
 
   describe("default input", () => {
     test("If the input is undefined, an error should be thrown", () => {
       const input = undefined;
-      expect(() => mOcKiNgCaSe(input)).toThrow(new Error("An input is required"));
+      expect(() => mockingcase(input)).toThrow(new Error("An input is required"));
     });
 
     test("For config option if the input is undefined, an error should be thrown", () => {
-      const mOcKiNgCaSe = require("./index").config({
+      const mockingcase = require("./index").config({
         onlyLetters: true,
       });
       const input = undefined;
       const options = {
         onlyLetters: false,
       };
-      expect(() => mOcKiNgCaSe(input, options)).toThrow(new Error("An input is required"));
+      expect(() => mockingcase(input, options)).toThrow(new Error("An input is required"));
     });
   });
 
   describe("overrideConsole", () => {
-    test("console.log always prints the mOcKiNgCaSe", () => {
+    test("console.log always prints the mockingcase", () => {
       const input = "hello world";
-      const expectedOutput = mOcKiNgCaSe(input);
+      const expectedOutput = mockingcase(input);
       let consoleOutput = null;
       console.log = jest.fn(output => {
         consoleOutput = output;
       });
-      mOcKiNgCaSe.overrideConsole();
+      mockingcase.overrideConsole();
       console.log(input);
       expect(consoleOutput).toEqual(expectedOutput);
     });
     test("overrideConsole can also be implemented while requiring", () => {
       const input = "hello world";
-      const mOcKiNgCaSe = require("./index").overrideConsole();
-      const expectedOutput = mOcKiNgCaSe(input);
+      const mockingcase = require("./index").overrideConsole();
+      const expectedOutput = mockingcase(input);
       let consoleOutput = null;
       console.log = jest.fn(output => {
         consoleOutput = output;
       });
-      mOcKiNgCaSe.overrideConsole();
+      mockingcase.overrideConsole();
       console.log(input);
       expect(consoleOutput).toEqual(expectedOutput);
     });
@@ -232,11 +232,11 @@ describe("mockingcase", () => {
       const options = {
         upper: "worl",
       };
-      let resultOutput = mOcKiNgCaSe(lowerCaseInput, options);
+      let resultOutput = mockingcase(lowerCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(capitalizeInput, options);
+      resultOutput = mockingcase(capitalizeInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(upperCaseInput, options);
+      resultOutput = mockingcase(upperCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
     });
 
@@ -245,11 +245,11 @@ describe("mockingcase", () => {
       const options = {
         upper: "[lwro]",
       };
-      let resultOutput = mOcKiNgCaSe(lowerCaseInput, options);
+      let resultOutput = mockingcase(lowerCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(capitalizeInput, options);
+      resultOutput = mockingcase(capitalizeInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(upperCaseInput, options);
+      resultOutput = mockingcase(upperCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
     });
 
@@ -258,11 +258,11 @@ describe("mockingcase", () => {
       const options = {
         upper: "ohd",
       };
-      let resultOutput = mOcKiNgCaSe(lowerCaseInput, options);
+      let resultOutput = mockingcase(lowerCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(capitalizeInput, options);
+      resultOutput = mockingcase(capitalizeInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(upperCaseInput, options);
+      resultOutput = mockingcase(upperCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
     });
   });
@@ -277,11 +277,11 @@ describe("mockingcase", () => {
       const options = {
         upper: /worl/,
       };
-      let resultOutput = mOcKiNgCaSe(lowerCaseInput, options);
+      let resultOutput = mockingcase(lowerCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(capitalizeInput, options);
+      resultOutput = mockingcase(capitalizeInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(upperCaseInput, options);
+      resultOutput = mockingcase(upperCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
     });
 
@@ -290,11 +290,11 @@ describe("mockingcase", () => {
       const options = {
         upper: /[lwro]/,
       };
-      let resultOutput = mOcKiNgCaSe(lowerCaseInput, options);
+      let resultOutput = mockingcase(lowerCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(capitalizeInput, options);
+      resultOutput = mockingcase(capitalizeInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(upperCaseInput, options);
+      resultOutput = mockingcase(upperCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
     });
 
@@ -303,11 +303,11 @@ describe("mockingcase", () => {
       const options = {
         upper: /ohd/,
       };
-      let resultOutput = mOcKiNgCaSe(lowerCaseInput, options);
+      let resultOutput = mockingcase(lowerCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(capitalizeInput, options);
+      resultOutput = mockingcase(capitalizeInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(upperCaseInput, options);
+      resultOutput = mockingcase(upperCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
     });
   });
@@ -317,28 +317,28 @@ describe("mockingcase", () => {
       const options = {
         upper: 312313,
       };
-      const invalidCall = mOcKiNgCaSe.bind(null, "hello world", options);
+      const invalidCall = mockingcase.bind(null, "hello world", options);
       expect(invalidCall).toThrow();
     });
     test("Should throw Error if set as boolean", () => {
       const options = {
         upper: true,
       };
-      const invalidCall = mOcKiNgCaSe.bind(null, "hello world", options);
+      const invalidCall = mockingcase.bind(null, "hello world", options);
       expect(invalidCall).toThrow();
     });
     test("Should throw Error if set as array", () => {
       const options = {
         upper: ["a", "s", "g"],
       };
-      const invalidCall = mOcKiNgCaSe.bind(null, "hello world", options);
+      const invalidCall = mockingcase.bind(null, "hello world", options);
       expect(invalidCall).toThrow();
     });
     test("Should throw Error if set as dict", () => {
       const options = {
         upper: { first: "a", second: "b" },
       };
-      const invalidCall = mOcKiNgCaSe.bind(null, "hello world", options);
+      const invalidCall = mockingcase.bind(null, "hello world", options);
       expect(invalidCall).toThrow();
     });
   });
@@ -353,11 +353,11 @@ describe("mockingcase", () => {
       const options = {
         lower: "WORL",
       };
-      let resultOutput = mOcKiNgCaSe(lowerCaseInput, options);
+      let resultOutput = mockingcase(lowerCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(capitalizeInput, options);
+      resultOutput = mockingcase(capitalizeInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(upperCaseInput, options);
+      resultOutput = mockingcase(upperCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
     });
 
@@ -366,11 +366,11 @@ describe("mockingcase", () => {
       const options = {
         lower: "[LWRO]",
       };
-      let resultOutput = mOcKiNgCaSe(lowerCaseInput, options);
+      let resultOutput = mockingcase(lowerCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(capitalizeInput, options);
+      resultOutput = mockingcase(capitalizeInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(upperCaseInput, options);
+      resultOutput = mockingcase(upperCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
     });
 
@@ -379,11 +379,11 @@ describe("mockingcase", () => {
       const options = {
         lower: "ohd",
       };
-      let resultOutput = mOcKiNgCaSe(lowerCaseInput, options);
+      let resultOutput = mockingcase(lowerCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(capitalizeInput, options);
+      resultOutput = mockingcase(capitalizeInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(upperCaseInput, options);
+      resultOutput = mockingcase(upperCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
     });
   });
@@ -398,11 +398,11 @@ describe("mockingcase", () => {
       const options = {
         lower: /WORL/,
       };
-      let resultOutput = mOcKiNgCaSe(lowerCaseInput, options);
+      let resultOutput = mockingcase(lowerCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(capitalizeInput, options);
+      resultOutput = mockingcase(capitalizeInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(upperCaseInput, options);
+      resultOutput = mockingcase(upperCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
     });
 
@@ -411,11 +411,11 @@ describe("mockingcase", () => {
       const options = {
         lower: /[OE]/,
       };
-      let resultOutput = mOcKiNgCaSe(lowerCaseInput, options);
+      let resultOutput = mockingcase(lowerCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(capitalizeInput, options);
+      resultOutput = mockingcase(capitalizeInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(upperCaseInput, options);
+      resultOutput = mockingcase(upperCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
     });
 
@@ -424,11 +424,11 @@ describe("mockingcase", () => {
       const options = {
         lower: /ohd/,
       };
-      let resultOutput = mOcKiNgCaSe(lowerCaseInput, options);
+      let resultOutput = mockingcase(lowerCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(capitalizeInput, options);
+      resultOutput = mockingcase(capitalizeInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
-      resultOutput = mOcKiNgCaSe(upperCaseInput, options);
+      resultOutput = mockingcase(upperCaseInput, options);
       expect(resultOutput).toStrictEqual(expectedOutput);
     });
   });
@@ -438,29 +438,32 @@ describe("mockingcase", () => {
       const options = {
         lower: 312313,
       };
-      const invalidCall = mOcKiNgCaSe.bind(null, "hello world", options);
+      const invalidCall = mockingcase.bind(null, "hello world", options);
       expect(invalidCall).toThrow();
     });
     test("Should throw Error if set as boolean", () => {
       const options = {
         lower: true,
       };
-      const invalidCall = mOcKiNgCaSe.bind(null, "hello world", options);
+      const invalidCall = mockingcase.bind(null, "hello world", options);
       expect(invalidCall).toThrow();
     });
     test("Should throw Error if set as array", () => {
       const options = {
         lower: ["a", "s", "g"],
       };
-      const invalidCall = mOcKiNgCaSe.bind(null, "hello world", options);
+      const invalidCall = mockingcase.bind(null, "hello world", options);
       expect(invalidCall).toThrow();
     });
     test("Should throw Error if set as dict", () => {
       const options = {
         lower: { first: "a", second: "b" },
       };
-      const invalidCall = mOcKiNgCaSe.bind(null, "hello world", options);
+      const invalidCall = mockingcase.bind(null, "hello world", options);
       expect(invalidCall).toThrow();
     });
   });
 });
+
+
+
